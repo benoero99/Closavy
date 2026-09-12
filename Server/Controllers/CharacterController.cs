f34b6cc70d869f365fb4783d8ce4b869bda8ba4f
Bence Hoffman <hoffman.bence@autsoft.hu>
2026-09-12 12:09:19 +0200

    get all characters for account fix

--- Server/Controllers/CharacterController.cs
+++ Server/Controllers/CharacterController.cs
@@ -44,13 +44,13 @@ public class CharacterController(
         return Ok(character);
     }
 
-    [HttpGet("{accountId:int}")]
+    [HttpGet("AccountId/{accountId:int}/Characters", Name = "GetCharactersByAccountId")]
     [ProducesResponseType(typeof(List<CharacterResponseDto>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<CharacterResponseDto>> GetCharactersByAccount([FromRoute] int accountId, CancellationToken ct = default)
+    public async Task<ActionResult<CharacterResponseDto>> GetCharactersByAccountId([FromRoute] int accountId, CancellationToken ct = default)
     {
-        CharacterResponseDto character = await characterService.GetCharacterAsync(characterId, ct);
+        List<CharacterResponseDto> character = await characterService.GetCharactersByAccountIdAsync(accountId, ct);
 
         return Ok(character);
     }
